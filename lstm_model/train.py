55da37b2525a36b3ff0b6aabd1971b268ba7ed92
Xuefeng Luo <hakunamatatahenry@gmail.com>
2019-12-16 13:14:41 +0100

    add batch_size

--- lstm_model/train.py
+++ lstm_model/train.py
@@ -4,6 +4,7 @@ from keras.utils import to_categorical
 from keras.models import Sequential
 from keras.layers import Dense, LSTM, Embedding
 import numpy as np
+from math import ceil
 
 
 def load_seq(file):
@@ -29,12 +30,22 @@ def model(cell, vocab_size, embed_dim, input_length):
     return model
 
 
-def data_generator(X, y, steps, vocab_size):
+def data_generator(X, y, input_length, batch_size, vocab_size):
     while True:
-        for i in range(steps):
-            X_out = np.array([X[i]])
-            y_out = np.array([to_categorical(y[i], num_classes=vocab_size)])
-            yield X_out, y_out
+        # for i in range(steps):
+        #     X_out = np.array([X[i]])
+        #     y_out = np.array([to_categorical(y[i], num_classes=vocab_size)])
+        #     yield X_out, y_out
+
+        iter = 0
+        while iter < input_length:
+            end = iter+batch_size
+            if end > input_length:
+                yield X[iter:], to_categorical(y[iter:], num_classes=vocab_size)
+                iter = end
+                break
+            yield X[iter:end], to_categorical(y[iter:end], num_classes=vocab_size)
+            iter = end
 
 
 if __name__ == '__main__':
@@ -45,6 +56,8 @@ if __name__ == '__main__':
         default=300, help='embedding layer dim')
     parser.add_argument('-lc', action="store", dest="lstm_cells", type=int,
         default=128, help='lstm cells')
+    parser.add_argument('-bs', action="store", dest="batch_size", type=int,
+        default=256, help='batch_size')
 
 
     args = parser.parse_args()
@@ -53,6 +66,7 @@ if __name__ == '__main__':
     sequences_path = args.sequences_path
     embed_dim = args.embed_dim
     lstm_cells = args.lstm_cells
+    batch_size = args.batch_size
 
     print('Load tokenizer...')
     tokenizer = load(open(tokenizer_path, 'rb'))
@@ -60,13 +74,15 @@ if __name__ == '__main__':
     sequences = load(open(sequences_path, 'rb'))
 
     # vocab_size = len(tokenizer.word_index) + 1
-    vocab_size = tokenizer.num_words
+    vocab_size = min((tokenizer.num_words, len(tokenizer.word_index) + 1))
 
     X, y = sequences[:, :-1], sequences[:, -1]
-    steps = len(X)
+    input_length = len(X)
     seq_length = X.shape[1]
+    samples_per_epoch = ceil(input_length/batch_size)
 
     model = model(lstm_cells, vocab_size, embed_dim, seq_length)
 
-    model.fit_generator(data_generator(X, y, steps, vocab_size),
-        samples_per_epoch=steps, nb_epoch=10)
+    model.fit_generator(data_generator(X, y, input_length, batch_size, vocab_size), samples_per_epoch=samples_per_epoch, nb_epoch=2)
+
+    model.save('../trained/demo_model.h5')
