b4f27349f6fc1fc14f4c64096940e7e721862b2f
Xuefeng Luo <hakunamatatahenry@gmail.com>
2019-12-17 16:04:37 +0100

    add top num

--- lstm_model/production.py
+++ lstm_model/production.py
@@ -10,12 +10,35 @@ from pickle import load
 import numpy as np
 import json
 
+def make_prob(predicts_raw, tokenizer, top_num):
+    prob_list = []
+    for predict in predicts_raw:
+        prob = dict()
+
+        top_index = np.argsort(predict)[::-1]
+        for i in range(top_num):
+            index = top_index[i]
+            if index == 0:
+                prob['||'] = str(predict[index])
+                continue
+
+            word = tokenizer.index_word[index]
+            prob[word] = str(predict[index])
+
+        prob_list.append(prob)
+
+    # hard code here
+    return [prob_list, prob_list]
+
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='produce prob')
     parser.add_argument('-cw', action="store", dest="context_win", type=int, default=5, help='set context windows')
     parser.add_argument('-t', action="store", dest="target", type=str, default='../data/tasaS.txt', help='set target path')
     parser.add_argument('-mp', action="store", dest="model_path", type=str, default='../trained/demo_model.h5', help='set trained model path')
     parser.add_argument('-tp', action="store", dest="tokenizer_path", type=str, default='../trained/tokenizer.pkl', help='tokenizer save path')
+    parser.add_argument('-tn', action="store", dest="top_num", type=int, default=100, help='top number of candidate to keep')
 
     args = parser.parse_args()
 
@@ -23,6 +46,7 @@ if __name__ == '__main__':
     target = args.target
     model_path = args.model_path
     tokenizer_path = args.tokenizer_path
+    top_num = args.top_num
 
     print('Load model...')
     model = load_model(model_path)
@@ -32,63 +56,24 @@ if __name__ == '__main__':
 
     tts = read_tasa(target)
 
-    # for tt in tts:
-    #     tt.demo_prob_table()
-    #     print("=====")
-    #     print(json.dumps(tt, cls=TasaTextEncoder, indent=4))
-    # quit()
-
     lines = prepare_sequences_tasatext(tts, context_win)
-    # words = [l.split() for l in lines]
-
-    # # print(words[1][:-1])
-    # # quit()
-    # contexts, golds = words[:][:-1], words[:][-1]
-
-    # print(contexts[2], golds[2])
-    # quit()
 
     sequences = tokenizer.texts_to_sequences(lines)
     sequences = np.array(sequences)
 
     X, y = sequences[:, :-1], sequences[:, -1]
 
-    # print(X.shape)
-    # print(y.shape)
-
     predicts_raw = model.predict(X)
-    # print(predicts_raw.shape)
 
-    aa = []
-    for predict in predicts_raw:
-        a = dict()
-        for i, j in enumerate(predict):
-            if i == 0:
-                a['||'] = str(j)
-                continue
-
-            word = tokenizer.index_word[i]
-            a[word] = str(j)
-        aa.append(a)
-
-    bb = [aa, aa]
-
-    # for i, (aa, tt, gold) in enumerate(zip(bb, tts, golds)):
-    #     # tt.demo_prob_table()
-    #     # tt.
-    #     # print("=====")
-    #     # print(json.dumps(tt, cls=TasaTextEncoder, indent=4))
-    #     pass
+    prob_list = make_prob(predicts_raw, tokenizer, top_num)
 
     length = len(tts)
 
     for i in range(length):
-        # print(i, bb[i], tts[i], golds[i])
-        # print("-----------")
         prob_table = []
-        position_length = len(bb[i])
+        position_length = len(prob_list[i])
         for j in range(position_length):
-            ttp = TasaTextProb(j, bb[i][j])
+            ttp = TasaTextProb(j, prob_list[i][j])
             prob_table.append(ttp)
 
         tts[i].prob_table = prob_table
