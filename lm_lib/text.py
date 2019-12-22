db99d1ce1147892873b4a6d775d42d8dd1c61b43
Xuefeng Luo <hakunamatatahenry@gmail.com>
2019-12-22 16:11:17 +0100

    update methods for production

--- lm_lib/text.py
+++ lm_lib/text.py
@@ -1,12 +1,14 @@
 from json import JSONEncoder
 from nltk.tokenize import word_tokenize
+import json
+import numpy as np
 
 class TasaText(object):
-    # __slots__ = ['description', 'sents', 'prob_table']
-
-    def __init__(self, descritpion, sents, prob_table=None):
-        self.description = descritpion
+    def __init__(self, name, description, sents, segments, prob_table=None):
+        self.name = name
+        self.description = description
         self.sents = sents
+        self.segments = segments
         if prob_table == None:
             self.prob_table = []
         else:
@@ -17,8 +19,14 @@ class TasaText(object):
         lines = text.replace('\n', '').split('[S]')
 
         if lines[0]:
-            return cls(lines[0], lines[1:], None)
+            name = lines[0].split()[0]
+            segments = word_tokenize(' '.join(lines[1:]))
+            return cls(name, lines[0], lines[1:], segments, None)
         return None
+
+    @classmethod
+    def from_tt(cls, tt):
+        return cls(tt.name, tt.description, tt.sents, tt.segments, tt.prob_table)
 
     def __str__(self):
         output = self.description
@@ -29,13 +37,13 @@ class TasaText(object):
         return output
 
     def to_sequences(self, context_win=5, open_closed_tag=True):
-        text = ' '.join(self.sents)
-        sequences = self.construct_sequences(text, context_win)
+        # text = ' '.join(self.sents)
+        sequences = self.construct_sequences(context_win)
 
         return sequences
 
-    def construct_sequences(self, text, context_win):
-        tokens = word_tokenize(text)
+    def construct_sequences(self, context_win):
+        tokens = self.segments
 
         length = context_win + 1
         sequences = list()
@@ -65,20 +73,58 @@ class TasaText(object):
 
         return sequences
 
+    def construct_prob_table(self, model, context_win, tokenizer, top_num):
+        lines = self.to_sequences(context_win)
+        sequences = tokenizer.texts_to_sequences(lines)
+        sequences = np.array(sequences)
+        X, y = sequences[:, :-1], sequences[:, -1]
 
-class TasaTextProb(object):
-    # __slots__ = ['position', 'gold', 'contexts', 'prob']
+        predicts_raw = model.predict(X)
 
-    def __init__(self, position, prob):
-        self.position = position
-        # self.gold = gold
-        # self.contexts = contexts
-        self.prob = prob
+        prob_table = self.construct_prob(predicts_raw, tokenizer, top_num)
+
+        prob_table_edited = {}
+        for i, prob in enumerate(prob_table):
+            token = {}
+            seq = lines[i].split()
+            contexts = ' '.join(seq[:context_win])
+            target = seq[-1]
+            token['contexts'] = contexts
+            token['target'] = target
+            token['prob_table'] = prob
+            prob_table_edited[i] = token
+
+        # output = json.dumps(prob_table_edited, indent=4)
+
+        # output_path =  '{}{}.json'.format(output_dir, self.name)
+        # with open(output_path, 'w+', encoding='utf8') as fp:
+        #     fp.write(output)
+
+        self.prob_table = prob_table_edited
+
+    def construct_prob(self, predicts_raw, tokenizer, top_num):
+        prob_table_list = []
+        for predict in predicts_raw:
+            prob_table = dict()
+
+            top_index = np.argsort(predict)[::-1]
+            for i in range(top_num):
+                index = top_index[i]
+                if index == 0:
+                    prob_table['<NULL>'] = str(predict[index])
+                    continue
+
+                word = tokenizer.index_word[index]
+                prob_table[word] = str(predict[index])
+
+            prob_table_list.append(prob_table)
+
+        return prob_table_list
 
 
 class TasaTextEncoder(JSONEncoder):
     def default(self, object):
-        if isinstance(object, TasaTextProb) or isinstance(object, TasaText):
+        if isinstance(object, TasaText):
             return object.__dict__
         else:
             # call base class implementation which takes care of
--- lstm_model/production.py
+++ lstm_model/production.py
@@ -2,35 +2,13 @@ import sys, os
 path = os.path.dirname(sys.path[0])
 sys.path.insert(0, path)
 
-from keras.models import load_model
 import argparse
-from lm_lib.read import read_tasa, prepare_sequences_tasatext
-from lm_lib.text import TasaTextEncoder, TasaTextProb
+from keras.models import load_model
 from pickle import load
-import numpy as np
+from lm_lib.read import read_tasa
+from lm_lib.text import TasaTextEncoder
+from tqdm import tqdm
 import json
-
-def make_prob(predicts_raw, tokenizer, top_num):
-    prob_list = []
-    for predict in predicts_raw:
-        prob = dict()
-
-        top_index = np.argsort(predict)[::-1]
-        for i in range(top_num):
-            index = top_index[i]
-            if index == 0:
-                prob['||'] = str(predict[index])
-                continue
-
-            word = tokenizer.index_word[index]
-            prob[word] = str(predict[index])
-
-        prob_list.append(prob)
-
-    # hard code here
-    return [prob_list, prob_list]
-
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='produce prob')
@@ -56,30 +34,10 @@ if __name__ == '__main__':
 
     tts = read_tasa(target)
 
-    lines = prepare_sequences_tasatext(tts, context_win)
+    for tt in tqdm(tts):
+        tt.construct_prob_table(model, context_win, tokenizer, top_num)
 
-    sequences = tokenizer.texts_to_sequences(lines)
-    sequences = np.array(sequences)
-
-    X, y = sequences[:, :-1], sequences[:, -1]
-
-    predicts_raw = model.predict(X)
-
-    prob_list = make_prob(predicts_raw, tokenizer, top_num)
-
-    length = len(tts)
-
-    for i in range(length):
-        prob_table = []
-        position_length = len(prob_list[i])
-        for j in range(position_length):
-            ttp = TasaTextProb(j, prob_list[i][j])
-            prob_table.append(ttp)
-
-        tts[i].prob_table = prob_table
-
-    output = json.dumps(tts, cls=TasaTextEncoder, indent=4)
-
-    with open('../trained/output.json', 'w+', encoding='utf8') as fp:
-        fp.write(output)
-    
+        output = json.dumps(tt, cls=TasaTextEncoder, indent=4)
+        output_path = '../output/{}.json'.format(tt.name)
+        with open(output_path, 'w+', encoding='utf8') as fp:
+            fp.write(output)
