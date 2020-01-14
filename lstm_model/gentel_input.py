6a4af19c0cc3ed83993f42b72a3c15374e7c74b2
Xuefeng Luo <hakunamatatahenry@gmail.com>
2020-01-14 21:58:24 +0100

    find all gentel_input file

---
+++ lstm_model/gentel_input.py
@@ -0,0 +1,28 @@
+import glob
+import os
+import argparse
+
+def findFiles(path):
+    return glob.glob(path)
+
+def save(path, files):
+    with open(path, 'w+', encoding='utf8') as fp:
+        output = '\n'.join(files)
+        fp.write(output)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='find all gentelinput file')
+    parser.add_argument('-dp', action="store", dest="data_path", type=str, default='/mnt/shared/projects/RedHen/well_aligned', help='set context windows')
+    parser.add_argument('-sp', action="store", dest="save_path", type=str, default='../trained/', help='set save path')
+
+    args = parser.parse_args()
+    data_path = args.data_path + '*.gentelinput'
+    save_path = args.save_path + 'gentelinput_list.txt'
+
+    files = []
+
+    for filename in findFiles(data_path):
+        file = os.path.splitext(os.path.basename(filename))[0]
+        files.append(file)
+
+    save(save_path, files)
