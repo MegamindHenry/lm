a7a2fb2c8585b50c0442ac175f3e055c7ef67672
Xuefeng Luo <hakunamatatahenry@gmail.com>
2020-01-19 16:45:14 +0100

    Update eval.py

--- lstm_model/eval.py
+++ lstm_model/eval.py
@@ -10,7 +10,7 @@ from keras.models import load_model
 from tqdm import tqdm
 
 
-def eval(model, X, gold, gi, k):
+def eval(model, X, gold, gi, k, tokenizer):
     total = 0
     correct = 0
 
@@ -18,12 +18,20 @@ def eval(model, X, gold, gi, k):
     predicts = argmax_top_k(predict_raw, k)
 
     for i in range(len(X)):
-        if gold[i] in predicts[i]:
+        # candidates = [tokenizer.index_word[j] for j in predicts[i]]
+        # gold_label = tokenizer.index_word[gold[i]]
+        # print(candidates)
+        # print(gold_label)
+
+        if gold[i] != 1 and gold[i] in predicts[i]:
             correct += 1
         total += 1
         
     accuracy = correct/total
-    return '{}: {}'.format(gi, accuracy)
+    result = '{}: {}'.format(gi, accuracy)
+    print(result)
+
+    return result
 
 
 if __name__ == '__main__':
@@ -31,7 +39,7 @@ if __name__ == '__main__':
     tokenizer_path = '../trained/tokenizer.pkl'
     model_path = '../trained/demo_model.h5'
     gentle_inputs_path = '../trained/gentleinput_list.txt'
-    k = 10
+    k = 100
 
     print('Read gentleinput list ...')
     gentle_inputs = read_gentleinput_list(gentle_inputs_path)
@@ -50,6 +58,6 @@ if __name__ == '__main__':
 
         X, y = sequences[:, :-1], sequences[:, -1]
 
-        results.append(eval(model, X, y, gi, k))
+        results.append(eval(model, X, y, gi, k, tokenizer))
 
     print(results)
