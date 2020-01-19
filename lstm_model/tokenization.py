da30abc9bf9806a499847770c7be17c08a4c005b
Xuefeng Luo <hakunamatatahenry@gmail.com>
2020-01-19 16:46:50 +0100

    add up to 20000

--- lstm_model/tokenization.py
+++ lstm_model/tokenization.py
@@ -13,7 +13,7 @@ from lm_lib.read import load_seq
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='prepare context words for tasa corpus')
-    parser.add_argument('-wn', action="store", dest="word_num", type=int, default=10000, help='only this number of word will be keept')
+    parser.add_argument('-wn', action="store", dest="word_num", type=int, default=20000, help='only this number of word will be keept')
     parser.add_argument('-f', action="store", dest="file", type=str, default='../trained/sequences_5.txt', help='set save path')
     parser.add_argument('-tp', action="store", dest="tokenizer_path", type=str, default='../trained/', help='set tokenizer save path')
 
