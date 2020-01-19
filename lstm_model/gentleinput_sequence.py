088d6e1ab9eb8ec1ce242261a2108aad81641a88
Xuefeng Luo <hakunamatatahenry@gmail.com>
2020-01-19 15:26:39 +0100

    split sequences into different files

--- lstm_model/gentleinput_sequence.py
+++ lstm_model/gentleinput_sequence.py
@@ -2,9 +2,11 @@ import sys, os
 path = os.path.dirname(sys.path[0])
 sys.path.insert(0, path)
 
+from pathlib import Path
 from tqdm import tqdm
 import string
 from lm_lib.read import read_gentleinput_list
+import argparse
 
 def read_gentle(file, remove_punc=True):
     fp = open(file, 'r', encoding='utf8')
@@ -46,29 +48,36 @@ def to_sequences(segments, context_win):
 
     return sequences
 
-def save_sequences(file_path, sequence_list):
-    lines = ['\n'.join(sequences) for sequences in sequence_list]
+def save_sequences(file_path, sequences):
     with open(file_path, 'w+', encoding='utf8') as fp:
-        fp.write('\n'.join(lines))
+        fp.write('\n'.join(sequences))
 
 
 if __name__ == '__main__':
     gentle_inputs_path = '../trained/gentleinput_list.txt'
-    # data_path = '../test_data/'
     data_path = '/mnt/shared/projects/RedHen/well_aligned/'
     save_path = '../trained/gentleinput_seq/'
     context_win = 5
 
-    gentle_inputs = read_gentleinput_list(gentle_inputs_path)
-    print(gentle_inputs)
-    quit()
+    parser = argparse.ArgumentParser(description='find all gentelinput and output a sequences of all gentleinput files')
+    parser.add_argument("-tl", help="test in local env", action="store_true", dest="test_local")
 
-    sequence_list = []
+
+    args = parser.parse_args()
+    test_local = args.test_local
+    if test_local:
+        data_path = '../test_data/'
+
+
+    gentle_inputs = read_gentleinput_list(gentle_inputs_path)
+
+    Path('../trained/gentleinput_seq/').mkdir(parents=True, exist_ok=True)
+
+    # sequence_list = []
     for file in tqdm(gentle_inputs):
         file_path = data_path + file + '.gentleinput'
         segments = read_gentle(file_path, True)
         sequences = to_sequences(segments, context_win)
-        sequence_list.append(sequences)
-
-    save_sequences(save_path, sequence_list)
+        seq_path = '{}{}_{}.txt'.format(save_path, file, context_win)
+        save_sequences(seq_path, sequences)
 
